7d9970ee5da028f2dfcb4bb4cd33895eea159fdc
Pedro Ivo <kuhnpedroivo@gmail.com>
2022-09-19 15:52:32 -0300

    melhorias em dijkstra

--- src/application/dijkstra.java
+++ src/application/dijkstra.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 
 import entities.Aresta;
 import entities.Vertice;
@@ -11,6 +14,7 @@ public class dijkstra {
         origem.setVerticeAnterior(null);
         origem.setDistanciaPercorrida(0);
         int distancia;
+        List<Vertice> nPercorridos;
         
         do{
             if (!origem.getList().isEmpty()){
@@ -26,30 +30,11 @@ public class dijkstra {
                 }
             }
             origem.setPercorrido(true);
-            origem = menorCaminho(listaVertice);
 
-        }while(verificaPercorridos(listaVertice));
-        
-    }
+            nPercorridos = listaVertice.stream().filter(v -> (v.getPercorrido()==false)).collect(Collectors.toList());
+            if(!nPercorridos.isEmpty())
+                origem = nPercorridos.stream().min(Comparator.comparing(Vertice::getDistanciaPercorrida)).get();
 
-    private static boolean verificaPercorridos(List<Vertice> listaVertice) {
-        for (Vertice vertice : listaVertice) {
-            if(!vertice.getPercorrido()){
-               return true; 
-            }
-        }
-        return false;       
-    }
-
-    private static Vertice menorCaminho(List<Vertice> listaVertice) {
-        Vertice aux = new Vertice();
-        for (Vertice vertice : listaVertice) {
-            if (vertice.getPercorrido() != true){
-                if(aux.getDistanciaPercorrida() > vertice.getDistanciaPercorrida()){
-                    aux = vertice;
-                }
-            }
-        }
-        return aux;
+        }while(!nPercorridos.isEmpty());
     }
 }
